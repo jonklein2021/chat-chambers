04fc60498de858d04c522c082631b0fcbdd0dc9d
Jontron <jonklein2021@gmail.com>
2023-12-25 23:20:31 -0500

    test

--- client/src/utils/socket.js
+++ client/src/utils/socket.js
@@ -2,6 +2,7 @@ import { io } from 'socket.io-client';
 require('dotenv');
 
 // "undefined" means the URL will be computed from the `window.location` object
-const URL = process.env.NODE_ENV === 'production' ? process.env.SERVER_URL : 'http://localhost:9000';
+// const URL = process.env.NODE_ENV === 'production' ? process.env.SERVER_URL : 'http://localhost:9000';
+const URL = 'https://i-socket-server.jontronwonton.repl.co/';
 
 export const socket = io(URL, { autoConnect: false });
