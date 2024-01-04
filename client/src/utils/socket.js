bda4271ef00f231bf676d59db35f743651eaca8e
Jon Klein <jonklein2021@gmail.com>
2024-01-03 23:28:43 -0500

    add server url

--- client/src/utils/socket.js
+++ client/src/utils/socket.js
@@ -2,6 +2,7 @@ import { io } from 'socket.io-client';
 require('dotenv');
 
 // "undefined" means the URL will be computed from the `window.location` object
-const URL = process.env.NODE_ENVIRONMENT === 'production' ? process.env.SERVER_URL : 'http://localhost:9000';
+// const URL = process.env.NODE_ENV === 'production' ? process.env.SERVER_URL : 'http://localhost:9000';
+const URL = 'https://c66a29e1-46be-430c-8eb3-a9b6e34c62cb-00-3tnlqub3r4tq9.picard.replit.dev/';
 
 export const socket = io(URL, { autoConnect: false });
