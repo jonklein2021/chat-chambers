835051a1d15b2f07a6d73447bae9567003a09a50
Jon Klein <jonklein2021@gmail.com>
2024-01-03 21:31:02 -0500

    init server branch

--- client/src/pages/404/NotFound.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function NotFound() {
-  return (
-    <h1 className='notfound-title'>Error 404: Room not found</h1>
-  );
-}
-
-export default NotFound;
--- client/src/utils/socket.js
+++
@@ -1,9 +0,0 @@
-import { io } from 'socket.io-client';
-require('dotenv');
-
-// "undefined" means the URL will be computed from the `window.location` object
-// const URL = process.env.NODE_ENVIRONMENT === 'production' ? process.env.SERVER_URL : 'http://localhost:9000';
-const URL = 'https://i-socket-server.jontronwonton.repl.co/';
-// const URL = 'http://localhost:9000';
-
-export const socket = io(URL, { autoConnect: false });
