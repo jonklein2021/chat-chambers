e78f87576d4bde79ad1b35fd4ff5f3c9f24a0fc1
Jontron <jonklein2021@gmail.com>
2024-01-09 13:59:23 -0500

    add dockerfile

--- client/src/utils/socket.js
+++ client/src/utils/socket.js
@@ -1,8 +1,6 @@
 import { io } from 'socket.io-client';
 require('dotenv');
 
-// "undefined" means the URL will be computed from the `window.location` object
-// const URL = process.env.NODE_ENV === 'production' ? process.env.SERVER_URL : 'http://localhost:9000';
-const URL = 'https://c66a29e1-46be-430c-8eb3-a9b6e34c62cb-00-3tnlqub3r4tq9.picard.replit.dev/';
+const URL = process.env.NODE_ENV === 'production' ? process.env.REACT_APP_SERVER_URL : 'http://localhost:9000';
 
 export const socket = io(URL, { autoConnect: false });
